5eb05b9fd227f2f5a0ae4146d59a163a50b7ffdc
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-06-14 13:57:23 -0300

    First commit

---
+++ testeops/src/main/java/com/felipe/testeops/TesteopsApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.testeops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteopsApplication.class, args);
+	}
+
+}
