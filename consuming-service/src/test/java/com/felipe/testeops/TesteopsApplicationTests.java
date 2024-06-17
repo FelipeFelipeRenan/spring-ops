a77367ef9f3e449c5047ef52e06fc4b4185e27f4
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-06-17 17:49:11 -0300

    style: Organizing scripts for AWS services

---
+++ consuming-service/src/test/java/com/felipe/testeops/TesteopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.testeops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ consuming-service/terraform/terra2.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  profile = "localstack"
+  region     = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+  }
+}
+
+resource "aws_dynamodb_table" "my_table" {
+  name         = "my_table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_sqs_queue" "my_queue" {
+  name = "my_queue"
+}
