83068a9383877219c7e0f871129900015bd883d8
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-06-24 18:30:28 -0300

    build: Adding service discovery and api gateway to docker compose file:

--- service-discovery/src/main/java/com/felipe/service_discovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/felipe/service_discovery/ServiceDiscoveryApplication.java
@@ -2,9 +2,12 @@ package com.felipe.service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
+
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
