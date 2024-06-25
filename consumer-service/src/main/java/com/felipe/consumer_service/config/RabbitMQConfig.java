93fea577a60b10bc9da0f31241f914970ad4c1db
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-06-25 18:42:15 -0300

    chore: Creating configuration file to RabbitMQ at consumer service

---
+++ consumer-service/src/main/java/com/felipe/consumer_service/config/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.felipe.consumer_service.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String ORDER_QUEUE = "orderQueue";
+    public static final String ORDER_EXCHANGE = "orderExchange";
+    public static final String ORDER_ROUTING_KEY = "orderRoutingKey";
+
+    @Bean
+    public Queue orderQueue(){
+        return new Queue(ORDER_QUEUE, false);
+    }
+
+    @Bean
+    public DirectExchange orderExchange(){
+        return new DirectExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue orderQueue, DirectExchange orderExchange){
+        return BindingBuilder.bind(orderExchange).to(orderExchange).with(ORDER_ROUTING_KEY);
+    }
+}
