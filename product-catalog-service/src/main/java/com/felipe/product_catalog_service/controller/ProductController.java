28316f43ae8d620d178cc313c448b64866052b96
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-09-08 14:12:53 -0300

    feat: adding swagger documentation

--- product-catalog-service/src/main/java/com/felipe/product_catalog_service/controller/ProductController.java
+++ product-catalog-service/src/main/java/com/felipe/product_catalog_service/controller/ProductController.java
@@ -6,32 +6,35 @@ import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.felipe.product_catalog_service.dto.CreateProductRequest;
-import com.felipe.product_catalog_service.dto.ProductResponse;
-import com.felipe.product_catalog_service.mapper.ProductMapper;
-import com.felipe.product_catalog_service.service.ProductService;
-
-import jakarta.validation.Valid;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.felipe.product_catalog_service.dto.CreateProductRequest;
+import com.felipe.product_catalog_service.dto.ProductResponse;
 import com.felipe.product_catalog_service.dto.UpdateProductRequest;
+import com.felipe.product_catalog_service.mapper.ProductMapper;
+import com.felipe.product_catalog_service.service.ProductService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/v1/api/products")
+@Tag(name = "Product Catalog API", description = "Endpoints para gerir o catálogo de produtos")
 public class ProductController {
 
     private final ProductService productService;
@@ -43,6 +46,7 @@ public class ProductController {
     }
 
     @GetMapping
+    @Operation(summary = "Lista todos produtos de forma paginada")
     public Flux<ProductResponse> getAllProducts(
             @RequestParam(name = "page", defaultValue = "0") int page,
             @RequestParam(name = "size", defaultValue = "10") int size,
@@ -71,6 +75,9 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Busca um produto pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Produto encontrado")
+    @ApiResponse(responseCode = "404", description = "Produto não encontrado", content = @Content)
     public Mono<ProductResponse> getProductById(@PathVariable Long id) {
         return productService.findById(id)
                 .map(productMapper::toResponse);
@@ -78,6 +85,9 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Cria um novo produto")
+    @ApiResponse(responseCode = "201", description = "Produto criado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados de entrada inválido", content = @Content)
     public Mono<ProductResponse> createProduct(@Valid @RequestBody Mono<CreateProductRequest> request) {
         return request
                 .flatMap(productService::create)
@@ -85,6 +95,9 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Autaliza um produto existente")
+    @ApiResponse(responseCode = "200", description = "Produto autalizado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados de entrada inválido", content = @Content)
     public Mono<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody UpdateProductRequest product) {
 
         return productService.update(id, product)
@@ -92,6 +105,9 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deleta um produto")
+    @ApiResponse(responseCode = "201", description = "Produto deletador com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados de entrada inválido", content = @Content)
     public Mono<Void> deleteProduct(@PathVariable Long id) {
         return productService.deleteById(id);
     }
