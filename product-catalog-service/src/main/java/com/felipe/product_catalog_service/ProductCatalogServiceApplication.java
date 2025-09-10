13d28f7119fe149697a38936382ca145d0980e69
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-09-10 15:50:58 -0300

    feat: adding native compilation to product catalog service

--- product-catalog-service/src/main/java/com/felipe/product_catalog_service/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/felipe/product_catalog_service/ProductCatalogServiceApplication.java
@@ -1,11 +1,15 @@
 package com.felipe.product_catalog_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration; // IMPORT
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@EnableAutoConfiguration(exclude = {RefreshAutoConfiguration.class})
 public class ProductCatalogServiceApplication {
 
 	public static void main(String[] args) {
