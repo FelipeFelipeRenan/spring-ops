4c513f38005082c04c8d149215d7eed30c1a3fdc
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-09-12 18:23:46 -0300

    revert: reverting api gateway to use normal jvm instead native compilation, due to some problems related to reflection

--- api-gateway/src/main/java/com/felipe/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/felipe/api_gateway/ApiGatewayApplication.java
@@ -1,14 +1,9 @@
 package com.felipe.api_gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
-import org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { RefreshAutoConfiguration.class, CompositeDiscoveryClientAutoConfiguration.class
-})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
